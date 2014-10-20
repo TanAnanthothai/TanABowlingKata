6cff99cbea1ca91bb32bb0e312f89e8b0c36d5ed
Tan Ananthothai <teerakorn.a@gmail.com>
2014-10-20 13:18:25 +0700

    BowlingKata

---
+++ lib/bowling_game.rb
@@ -0,0 +1,33 @@
+class BowlingGame
+
+	def initialize
+		@rolls = [] # initialize an array to hold the score
+	end
+	
+	def roll pins
+		@rolls.push pins
+	end
+
+	def score
+		result = 0
+		rollIndex = 0
+		10.times do 
+			if @rolls[rollIndex] + @rolls[rollIndex+1] ==10
+				result += 10 + @rolls[rollIndex+2] 
+				rollIndex += 2
+			elsif @rolls[rollIndex]==10
+				result += 10 + @rolls[rollIndex+1] + @rolls[rollIndex+2] 
+				rollIndex += 1
+			else 
+				result += @rolls[rollIndex] + @rolls[rollIndex+1]
+				rollIndex += 2
+			end
+			
+		end
+		result
+	end
+
+end
+
+
+
---
+++ spec/lib/bowling_game_spec.rb
@@ -0,0 +1,55 @@
+require "spec_helper"
+require "bowling_game"   # name of the class we have just created
+ 
+describe 'Bowling Game Checking in case that the game is' do
+
+	before do 
+		puts "\nNew Bowling game is created"
+		@game = BowlingGame.new
+	end
+
+
+	it('all 0 games') do
+		puts "\nall 0 games"
+		game = BowlingGame.new
+		20.times{game.roll 0}
+		expect(game.score).to eq 0 
+	end
+
+	it('all ones') do
+		puts "\nall ones games"fdsafdsaf ds
+		game = BowlingGame.new
+		20.times{game.roll 1}
+		expect(game.score).to eq 20
+	end
+
+	it('a spare') do
+		puts "\na spare"
+		@game.roll 5
+		@game.roll 5
+		@game.roll 3
+		17.times{@game.roll 0}
+		expect(@game.score).to eq 16
+	end 
+
+	it('a strike') do
+		puts "\na strike"
+		@game.roll 10
+		@game.roll 4
+		@game.roll 3
+		16.times{@game.roll 0}
+		expect(@game.score).to eq 24
+	end 
+
+	it('all strikes') do
+		puts "\nall strikes"
+		12.times{@game.roll 10}
+		expect(@game.score).to eq 300
+	end
+
+	it('all spares') do
+		puts "\nall spares"
+		21.times{@game.roll 5}
+		expect(@game.score).to eq 150
+	end 
+end
